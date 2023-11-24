31d108cb982a9f0e3a6127dc63586c7fe4b70b59
Stonehagen <steinhagen@mailbox.org>
2023-11-24 08:59:52 +0100

    add startscript for gfs forecast

---
+++ src/gfs.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: __dirname + '/../.env' });
+const { updateDatabase } = require('./update_database');  
+
+// eslint-disable-next-line operator-linebreak
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+// eslint-disable-next-line no-console
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const wgrib2 = false;
+const forecastMap = true;
+updateDatabase('gfs', wgrib2, forecastMap).then((res) => db.close());
