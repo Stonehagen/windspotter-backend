f360533331723153224edc699a98a293525d561d
Stonehagen <steinhagen@mailbox.org>
2023-11-24 08:57:47 +0100

    add config fore gfs forecast

--- src/config/gfs.js
+++ src/config/gfs.js
@@ -1,13 +1,10 @@
 const gfs = {
   forecastName: 'gfs',
-  // check values dataValues: ['t_2m', 'v_10m', 'u_10m', 'vmax_10m', 'clct_mod', 'rain_gsp'],
-  // check name fCModel: 'regular-lat-lon_',
-  // check height fCHeight: '_2d_',
+  dataValues: ['t_2m', 'v_10m', 'u_10m', 'vmax_10m', 'clct_mod', 'rain_gsp'],
   serverDataTimeDelay: 5 * 60 * 1000,
-  server: 'https://www.ncei.noaa.gov/',
+  server: 'https://www.ncei.noaa.gov',
   dict: 'data/global-forecast-system/access/grid-004-0.5-degree/forecast/',
-  // set the regext for the name regexNameValue: /(?<=_[0-9]+_[0-9]+_[a-zA-Z0-9]+_).+(?=\.grib)/,
-  // set regex for the time regexTimeValue: /(?<=_[0-9]+_)[0-9]+(?=_[a-zA-Z0-9]+_.+grib)/,
+  regexRefTimeValue: /(?<=[a-zA-Z0-9]_[0-9]_[0-9]+_)[0-9]+(?=_[0-9]+\.grib)/,
 }
 
 module.exports = {
--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 const { iconD2 } = require('./iconD2');
 const { iconEu } = require('./iconEu');
 const { cwam } = require('./cwam');
+const { gfs } = require('./gfs');
 
 module.exports = {
   iconD2,
   cwam,
   iconEu,
+  gfs,
 };
